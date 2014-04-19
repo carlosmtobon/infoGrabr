1eb478e841ff4ca7ff4528e448afc81678f532ec
Carlos Tobon <ctobo003@fiu.edu>
2014-04-19 17:04:33 -0400

    --Added recordViewController --Deleted auto gen controller

---
+++ InfoGrabr/RecordViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecordViewController.h
+//  InfoGrabr
+//
+//  Created by Charles on 4/19/14.
+//  Copyright (c) 2014 Florida International University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecordViewController : UIViewController
+
+@end
--- InfoGrabr/RecordViewController.m
+++ InfoGrabr/RecordViewController.m
@@ -1,27 +1,32 @@
+//
+//  RecordViewController.m
+//  InfoGrabr
+//
+//  Created by Charles on 4/19/14.
+//  Copyright (c) 2014 Florida International University. All rights reserved.
+//
 
+#import "RecordViewController.h"
 
-#import "InfoGrabrSecondViewController.h"
-
-@interface InfoGrabrSecondViewController ()
+@interface RecordViewController ()
 
 @end
 
-@implementation InfoGrabrSecondViewController
+@implementation RecordViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        self.title = NSLocalizedString(@"Second", @"Second");
-        self.tabBarItem.image = [UIImage imageNamed:@"second"];
+        // Custom initialization
     }
     return self;
 }
-							
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view from its nib.
 }
 
 - (void)didReceiveMemoryWarning
