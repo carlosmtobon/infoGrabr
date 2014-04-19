4d759d7a86dd348bd0a21508b3c9819a28acdc51
prhalsall <prhalsall@gmail.com>
2014-04-19 18:56:16 -0400

    add instance variables for textfields, implement pickerview on a keyboard for the timeframe

--- InfoGrabr/QuestionnaireViewController.h
+++ InfoGrabr/QuestionnaireViewController.h
@@ -9,11 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @class QuestionnaireServices;
+@class Attendee;
 
-@interface QuestionnaireViewController : UIViewController <UIPickerViewDataSource,UIPickerViewDelegate>
+@interface QuestionnaireViewController : UIViewController <UIPickerViewDataSource,UIPickerViewDelegate, UITextFieldDelegate, UITextViewDelegate>
 {
     NSInteger originalCount;
     QuestionnaireServices *sharedServices;
+    Attendee *clientLead;
 }
 
 @property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
@@ -23,5 +25,32 @@
 @property (strong, nonatomic) IBOutlet UIPickerView *servicesPicker;
 
 @property (strong, nonatomic) NSMutableArray *services;
+
+@property (strong, nonatomic) NSMutableArray *timeframes;
+
+@property (strong, nonatomic) UITextField *timeFramePickerView;
+
+@property (strong, nonatomic) Attendee *clientLead;
+
+- (IBAction)saveButton:(id)sender;
+
+- (IBAction)timeFrameDropDown:(id)sender;
+
+@property (strong, nonatomic) IBOutlet UITextField *confID;
+@property (strong, nonatomic) IBOutlet UITextField *firstName;
+@property (strong, nonatomic) IBOutlet UITextField *lastName;
+@property (strong, nonatomic) IBOutlet UITextField *organization;
+@property (strong, nonatomic) IBOutlet UITextView *notes;
+@property (strong, nonatomic) IBOutlet UITextField *company;
+@property (strong, nonatomic) IBOutlet UITextField *office;
+@property (strong, nonatomic) IBOutlet UITextField *address;
+@property (strong, nonatomic) IBOutlet UITextField *city;
+@property (strong, nonatomic) IBOutlet UITextField *state;
+@property (strong, nonatomic) IBOutlet UITextField *zipcode;
+@property (strong, nonatomic) IBOutlet UITextField *country;
+@property (strong, nonatomic) IBOutlet UITextField *membership;
+@property (strong, nonatomic) IBOutlet UITextField *phoneOne;
+@property (strong, nonatomic) IBOutlet UITextField *phoneTwo;
+@property (strong, nonatomic) IBOutlet UITextField *email;
 
 @end
--- InfoGrabr/QuestionnaireViewController.m
+++ InfoGrabr/QuestionnaireViewController.m
@@ -8,17 +8,19 @@
 
 #import "QuestionnaireViewController.h"
 #import "QuestionnaireServices.h"
+#import "Attendee.h"
 
 @implementation QuestionnaireViewController
 
-@synthesize servicesPicker;
-@synthesize services;
+@synthesize servicesPicker, timeFramePickerView;
+@synthesize services, timeframes;
+@synthesize clientLead;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        self.title = NSLocalizedString(@"Second", @"Second");
+        self.title = NSLocalizedString(@"Questionnaire", @"Questionnaire");
         self.tabBarItem.image = [UIImage imageNamed:@"second"];
     }
     return self;
@@ -33,6 +35,57 @@
     services = [sharedServices services];
     
     originalCount = services.count;
+    
+    timeframes = [NSArray arrayWithObjects:@"0-3 Months", @"6 Months", @"1 Year", @"Greater than 1 Year", nil];
+    
+    
+    self.timeFramePickerView = [[UITextField alloc] initWithFrame:CGRectZero];
+    [self.view addSubview:self.timeFramePickerView];
+    
+    UIPickerView *pickerView = [[UIPickerView alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
+    pickerView.showsSelectionIndicator = YES;
+    pickerView.dataSource = self;
+    pickerView.delegate = self;
+    
+    // set change the inputView (default is keyboard) to UIPickerView
+    self.timeFramePickerView.inputView = pickerView;
+    
+    // add a toolbar with Cancel & Done button
+    UIToolbar *toolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
+    toolBar.barStyle = UIBarStyleBlackOpaque;
+    
+    UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(doneTouched:)];
+    UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancelTouched:)];
+    
+    // the middle button is to make the Done button align to right
+    [toolBar setItems:[NSArray arrayWithObjects:cancelButton, [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil], doneButton, nil]];
+    self.timeFramePickerView.inputAccessoryView = toolBar;
+    
+    
+    self.confID.delegate = self;
+    self.firstName.delegate = self;
+    self.lastName.delegate = self;
+    self.organization.delegate = self;
+    self.notes.delegate = self;
+    self.company.delegate = self;
+    self.office.delegate = self;
+    self.address.delegate = self;
+    self.city.delegate = self;
+    self.state.delegate = self;
+    self.zipcode.delegate = self;
+    self.country.delegate = self;
+    self.membership.delegate = self;
+    self.phoneOne.delegate = self;
+    self.phoneTwo.delegate = self;
+    self.email.delegate = self;
+    
+    if (clientLead != nil)
+    {
+        self.confID.text = clientLead.confId;
+        self.firstName.text = clientLead.firstName;
+        self.lastName.text = clientLead.lastName;
+        self.organization.text = clientLead.organization;
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -47,43 +100,72 @@
     self.scrollView.contentSize = self.contentView.bounds.size;
 }
 
+
+//The number of columns. Both UIPickerViews have only 1 component
 - (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
     return 1;
 }
 
 -(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
-    return originalCount;
+    if ([pickerView isEqual:servicesPicker])
+        return originalCount;
+    else
+        return [timeframes count];
 }
 
 -(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
-    return [[services objectAtIndex:row] stringValue];
+    if ([pickerView isEqual:servicesPicker])
+        return [[services objectAtIndex:row] stringValue];
+    else
+    {
+        return [[timeframes objectAtIndex:row] stringValue];
+    }
 }
 
 -(UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view {
-    UITableViewCell *cell = (UITableViewCell *)view;
     
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
-        [cell setBackgroundColor:[UIColor clearColor]];
-        [cell setBounds: CGRectMake(0, 0, cell.frame.size.width -20 , 44)];
-        UITapGestureRecognizer *singleTapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(toggleSelection:)];
-        singleTapGestureRecognizer.numberOfTapsRequired = 1;
-        [cell addGestureRecognizer:singleTapGestureRecognizer];
+    if ([pickerView isEqual:servicesPicker])
+    {
+        UITableViewCell *cell = (UITableViewCell *)view;
+    
+        if (cell == nil) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+            [cell setBackgroundColor:[UIColor clearColor]];
+            [cell setBounds: CGRectMake(0, 0, cell.frame.size.width -20 , 44)];
+            UITapGestureRecognizer *singleTapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(toggleSelection:)];
+            singleTapGestureRecognizer.numberOfTapsRequired = 1;
+            [cell addGestureRecognizer:singleTapGestureRecognizer];
+        }
+    
+        if ([services indexOfObject:[NSNumber numberWithInt:row]] != NSNotFound) {
+            UIImageView *checkmark = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"checkbox-checked.png"]];
+            [cell setAccessoryView:checkmark];
+            //[cell setAccessoryType:UITableViewCellAccessoryCheckmark];
+        } else {
+            UIImageView *checkmark = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"checkbox.png"]];
+            [cell setAccessoryView:checkmark];
+            //[cell setAccessoryType:UITableViewCellAccessoryNone];
+        }
+        cell.textLabel.text = [services objectAtIndex:row];
+        cell.tag = row;
+    
+        return cell;
     }
-    
-    if ([services indexOfObject:[NSNumber numberWithInt:row]] != NSNotFound) {
-        UIImageView *checkmark = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"checkbox-checked.png"]];
-        [cell setAccessoryView:checkmark];
-        //[cell setAccessoryType:UITableViewCellAccessoryCheckmark];
-    } else {
-        UIImageView *checkmark = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"checkbox.png"]];
-        [cell setAccessoryView:checkmark];
-        //[cell setAccessoryType:UITableViewCellAccessoryNone];
+    else
+    {
+        UITableViewCell *cell = (UITableViewCell *)view;
+        
+        if (cell == nil) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+            [cell setBackgroundColor:[UIColor clearColor]];
+            [cell setBounds: CGRectMake(0, 0, cell.frame.size.width -20 , 44)];
+        }
+        
+        cell.textLabel.text = [timeframes objectAtIndex:row];
+        cell.tag = row;
+        
+        return cell;
     }
-    cell.textLabel.text = [services objectAtIndex:row];
-    cell.tag = row;
-    
-    return cell;
 }
 
 - (void)toggleSelection:(UITapGestureRecognizer *)recognizer {
@@ -98,6 +180,31 @@
         UIImageView *checkmark = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"checkbox-checked.png"]];
         [(UITableViewCell *)(recognizer.view) setAccessoryView:checkmark];//setAccessoryType:UITableViewCellAccessoryCheckmark];
     }
+}
+
+- (IBAction)saveButton:(id)sender {
+    
+    
+}
+
+- (IBAction)timeFrameDropDown:(id)sender {
+    [self.timeFramePickerView becomeFirstResponder];
+}
+
+- (void)cancelTouched:(UIBarButtonItem *)sender
+{
+    [self.timeFramePickerView resignFirstResponder];
+}
+
+- (void)doneTouched:(UIBarButtonItem *)sender
+{
+    [self.timeFramePickerView resignFirstResponder];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
 }
 
 @end
